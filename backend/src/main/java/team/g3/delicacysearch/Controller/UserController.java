ec394c2876ee8d9e911b0d39c4ef60f453891777
supinglan <s2530555829@outlook.com>
2023-12-15 13:35:43 +0800

    feature(backend): support search recommendation

---
+++ backend/src/main/java/team/g3/delicacysearch/Controller/UserController.java
@@ -0,0 +1,27 @@
+package team.g3.delicacysearch.Controller;
+
+import team.g3.delicacysearch.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RequestMapping("/user")
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public Map<String,Object> login(String username, String password) {
+
+        return userService.login(username, password);
+    }
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public Map<String, Object> register( String email, String username, String password) {
+        System.out.println(email);
+        System.out.println(username);
+        System.out.println(password);
+        return userService.register(email,username, password);
+    }
+}
